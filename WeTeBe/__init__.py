9b17425231db3bb89d1b01d22560480df36220f3
IgooGG <157390108+IgooGG@users.noreply.github.com>
2025-04-22 17:27:10 +0200

    Primary PRE-ALPHA release
    First ever release of WeTeBe package for python. It is importable using pip

---
+++ WeTeBe/__init__.py
@@ -0,0 +1,8 @@
+import builtins
+
+def print(message):
+    """
+    Print the given message to the standard output.
+    Works like the built-in print function but only accepts one argument.
+    """
+    builtins.print(message)
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='WeTeBe',
+    version='0.1.0',
+    description='An addon thats still in alpha. We will develop it soon',
+    author='IgooGG',
+    author_email='igo@igocheap.run.place',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
